4c04d60a67acc25395e00c8d5de119b6cfb95491
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-05-29 20:36:57 +0100

    initial

---
+++ test/test.js
@@ -0,0 +1,18 @@
+'use strict'
+const TestRunner = require('test-runner')
+const ArrayBase = require('../')
+const a = require('assert')
+
+const runner = new TestRunner()
+
+runner.test('main', function () {
+  class Arr extends ArrayBase {}
+
+  const arr = new Arr()
+  a.strictEqual(arr.length, 0)
+  arr.load([ 1,2,3 ])
+  a.strictEqual(arr.length, 3)
+  a.deepEqual(arr, [ 1,2,3 ])
+  arr.clear()
+  a.deepEqual(arr, [])
+})
